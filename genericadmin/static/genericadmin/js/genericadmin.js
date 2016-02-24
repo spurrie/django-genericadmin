42068dcfb118460aafed50ddb12428862f0aa6d0
Vincent Spanjersberg <vincent@nextgear.nl>
2016-02-24 15:07:45 +0100

    fix urls for django 1.9

--- genericadmin/static/genericadmin/js/genericadmin.js
+++ genericadmin/static/genericadmin/js/genericadmin.js
@@ -12,10 +12,8 @@
     var GenericAdmin = {
         url_array: null,
         fields: null,
-        obj_url: "../obj-data/",
-        admin_media_url: window.__admin_media_prefix__,
-		popup: '_popup',
-        
+        popup: '_popup',
+
         prepareSelect: function(select) {
             var that = this,
                 opt_keys = [],
@@ -29,8 +27,8 @@
 
                 if (this.value) {
                     if (that.url_array[this.value]) {
-                        key = that.url_array[this.value][0].split('/')[0];
-                        
+                        key = that.url_array[this.value][0].split('/')[2];
+
                         opt = $(this).clone();
                         opt.text(that.capFirst(opt.text()));
                         if ($.inArray(key, opt_keys) < 0) {
@@ -47,7 +45,7 @@
                 }
             });
             select.empty().append(no_value);
-            
+
             opt_keys = opt_keys.sort();
 
             $.each(opt_keys, function(index, key) {
@@ -62,7 +60,7 @@
         },
 
         getLookupUrlParams: function(cID) {
-            var q = this.url_array[cID][1] || {}, 
+            var q = this.url_array[cID][1] || {},
                 str = [];
             for(var p in q) {
                 str.push(encodeURIComponent(p) + "=" + encodeURIComponent(q[p]));
@@ -71,11 +69,11 @@
             url = x ? ("?" + x) : "";
             return url;
         },
-        
+
         getLookupUrl: function(cID) {
-            return '../../../' + this.url_array[cID][0] + '/' + this.getLookupUrlParams(cID);
+            return this.url_array[cID][0] + this.getLookupUrlParams(cID);
         },
-        
+
         getFkId: function() {
             if (this.fields.inline === false) {
                 return 'id_' + this.fields.fk_field;
@@ -83,7 +81,7 @@
                 return ['id_', this.fields.prefix, '-', this.fields.number, '-', this.fields.fk_field].join('');
             }
         },
-        
+
         getCtId: function() {
             if (this.fields.inline === false) {
                 return 'id_' + this.fields.ct_field;
@@ -91,25 +89,24 @@
                 return ['id_', this.fields.prefix, '-', this.fields.number, '-', this.fields.ct_field].join('');
             }
         },
-        
+
         capFirst: function(string) {
             return string.charAt(0).toUpperCase() + string.slice(1);
         },
-        
+
         hideLookupLink: function() {
             var this_id = this.getFkId();
             $('#lookup_' + this_id).unbind().remove();
             $('#lookup_text_' + this_id + ' a').remove();
             $('#lookup_text_' + this_id + ' span').remove();
         },
-        
+
         showLookupLink: function() {
             var that = this,
                 url = this.getLookupUrl(this.cID),
                 id = 'lookup_' + this.getFkId(),
-                link = '<a class="related-lookup" id="' + id + '" href="' + url + '">';
-                
-            link = link + '<img src="' + this.admin_media_url.replace(/\/?$/, '/') + 'img/selector-search.gif" style="cursor: pointer; margin-left: 5px; margin-right: 10px;" width="16" height="16" alt="Lookup"></a>';
+                link = '<a class="related-lookup" id="' + id + '" href="' + url + '"></a>';
+
             link = link + '<strong id="lookup_text_'+ this.getFkId() +'" margin-left: 5px"><a target="_new" href="#"></a><span></span></strong>';
 
             // insert link html after input element
@@ -117,7 +114,7 @@
 
             return id;
         },
-        
+
         pollInputChange: function(window) {
             var that = this,
                 interval_id = setInterval(function() {
@@ -129,23 +126,23 @@
                 },
                 150);
         },
-        
+
         popRelatedObjectLookup: function(link) {
             var name = id_to_windowname(this.getFkId()),
-				url_parts = [],
-                href, 
+                url_parts = [],
+                href,
                 win;
 
             if (link.href.search(/\?/) >= 0) {
-				url_parts[0] = '&';
+                url_parts[0] = '&';
                 //href = link.href + '&pop=1';
             } else {
-				url_parts[0] = '?';
+                url_parts[0] = '?';
                 //href = link.href + '?pop=1';
             }
-			url_parts[1] = this.popup;
-			url_parts[2] = '=1';
-			href = link.href + url_parts.join('');
+            url_parts[1] = this.popup;
+            url_parts[2] = '=1';
+            href = link.href + url_parts.join('');
             win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
 
             // wait for popup to be closed and load object data
@@ -154,19 +151,19 @@
             win.focus();
             return false;
         },
-        
+
         updateObjectData: function() {
             var that = this;
             return function() {
                 var value = that.object_input.val();
-                
-                if (!value) { 
-                    return 
+
+                if (!value) {
+                    return
                 }
                 //var this_id = that.getFkId();
                 $('#lookup_text_' + that.getFkId() + ' span').text('loading...');
                 $.ajax({
-                    url: that.obj_url,
+                    url: window.genericadmin.obj_url,
                     dataType: 'json',
                     data: {
                         object_id: value,
@@ -205,11 +202,11 @@
 
             this.url_array = url_array;
             this.fields = fields;
-			this.popup = popup_var || this.popup;
-            
+            this.popup = popup_var || this.popup;
+
             // store the base element
             this.object_input = $("#" + this.getFkId());
-            
+
             // find the select we need to change
             this.object_select = this.prepareSelect($("#" + this.getCtId()));
 
@@ -243,22 +240,22 @@
             this.updateObjectData()();
         }
     };
-    
+
     var InlineAdmin = {
         sub_admins: null,
         url_array: null,
         fields: null,
-		popup: '_popup',
-        
+        popup: '_popup',
+
         install: function(fields, url_array, popup_var) {
             var inline_count = $('#id_' + fields.prefix + '-TOTAL_FORMS').val(),
                 admin;
-            
+
             this.url_array = url_array;
             this.fields = fields;
             this.sub_admins = [];
-			this.popup = popup_var || this.popup;
-            
+            this.popup = popup_var || this.popup;
+
             for (var j = 0; j < inline_count; j++) {
                 f = $.extend({}, this.fields);
                 f.number = j;
@@ -277,7 +274,7 @@
                 added_fields.number = ($('#id_' + that.fields.prefix + '-TOTAL_FORMS').val() - 1);
                 admin.install(added_fields, that.url_array, that.popup);
                 that.sub_admins.push(admin);
-                
+
                 $('#' + that.fields.prefix + '-' + added_fields.number + ' .inline-deletelink').click(
                     that.removeHandler(that)
                 );
@@ -288,7 +285,7 @@
                 var parent_id,
                     deleted_num,
                     sub_admin;
-                
+
                 e.preventDefault();
                 parent_id = $(e.currentTarget).parents('.dynamic-' + that.fields.prefix).first().attr('id');
                 deleted_num = parseInt(parent_id.charAt(parent_id.length - 1), 10);
@@ -305,24 +302,26 @@
     };
 
     $(document).ready(function() {
-        $.ajax({
-            url: '../genericadmin-init/',
-            dataType: 'json',
-            success: function(data) {
-                var url_array = data.url_array,
-                    ct_fields = data.fields,
-					popup_var = data.popup_var,
-                    fields;
-                    
-                for (var i = 0; i < ct_fields.length; i++) {
-                    fields = ct_fields[i];
-                    if (fields.inline === false) {
-                        $.extend({}, GenericAdmin).install(fields, url_array, popup_var);
-                    } else {
-                        $.extend({}, InlineAdmin).install(fields, url_array, popup_var);
+        if ($('div.related-widget-wrapper').length > 0){
+            $.ajax({
+                url: window.genericadmin.init_url,
+                dataType: 'json',
+                success: function(data) {
+                    var url_array = data.url_array,
+                        ct_fields = data.fields,
+                        popup_var = data.popup_var,
+                        fields;
+
+                    for (var i = 0; i < ct_fields.length; i++) {
+                        fields = ct_fields[i];
+                        if (fields.inline === false) {
+                            $.extend({}, GenericAdmin).install(fields, url_array, popup_var);
+                        } else {
+                            $.extend({}, InlineAdmin).install(fields, url_array, popup_var);
+                        }
                     }
                 }
-            }
-        });
+            });
+        }
     });
 } (django.jQuery));
